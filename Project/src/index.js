d259aa1fa73a89c7c79a24a16d2007d16ce2e701
tessaflower <tessaleighflower@gmail.com>
2024-02-05 12:41:31 -0500

    Uploaded weather app project

---
+++ Project/src/index.js
@@ -0,0 +1,53 @@
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay}, ${hours}:${minutes}`;
+}
+let currentDateElement = document.querySelector(".current-details .date-time");
+let currentDate = new Date();
+currentDateElement.innerHTML = formatDate(currentDate);
+
+function citySelected(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-searched");
+  let currentCity = document.querySelector(".current-city");
+  let city = inpt.value;
+  currentCity.innerHTML = input.value;
+
+  let city = input.value;
+  let apiKey = `1a747f2d7ac32a100bt13fab8776o6ca`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then((response) => displayWeather(response, city));
+}
+
+function displayWeather(response, city) {
+  let temperature = Math.round(response.data.temperature.current);
+  let country = response.data.country;
+
+  let temperatureElement = document.querySelector(".current-temp-number");
+  temperatureElement.innerHTML = `${temperature}`;
+}
+
+let citySearch = document.querySelector("#search-form");
+citySearch.addEventListener("submit", citySelected);
